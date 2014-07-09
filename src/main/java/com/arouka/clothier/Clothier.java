7c17a537745164e0282ed567bc5ee1abdc087b38
Arouka <Seradar97@gmail.com>
2014-07-08 22:43:43 -0600

    Mcmod.info done 10/10 best mod ever

---
+++ src/main/java/com/arouka/clothier/Clothier.java
@@ -0,0 +1,33 @@
+package com.arouka.clothier;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid="clothier", name="Clothier", version ="0.1-dev")
+public class Clothier {
+
+    @Mod.Instance("Clothier")
+    public static Clothier instance;
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event)
+    {
+
+    }
+
+
+}
